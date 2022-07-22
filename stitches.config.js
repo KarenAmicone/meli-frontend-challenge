630366adcf998e78ee1774598a63b079b55be798
KarenAmicone <karen_amicone@hotmail.com>
2022-07-21 21:28:05 -0500

    Improving theme color name

--- stitches.config.js
+++ stitches.config.js
@@ -14,7 +14,7 @@ export const {
 		colors: {
 			primary: "#EFE60C",
 			secondary: "#333333",
-			neutralDarker: "#666666",
+			neutralDark: "#666666",
 			neutral: "#999999",
 			neutralLight: "#EEEEEE",
 			accent: "#3883FA",
