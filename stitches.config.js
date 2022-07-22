fb3a55361e60a5aef6a8884feda4683fa624b50d
KarenAmicone <karen_amicone@hotmail.com>
2022-07-21 19:30:27 -0500

    Initial configuration

---
+++ stitches.config.js
@@ -0,0 +1,23 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+	styled,
+	css,
+	globalCss,
+	keyframes,
+	getCssText,
+	theme,
+	createTheme,
+	config,
+} = createStitches({
+	theme: {
+		colors: {
+			primary: "#EFE60C",
+			secondary: "#333333",
+			neutralDarker: "#666666",
+			neutral: "#999999",
+			neutralLight: "#EEEEEE",
+			accent: "#3883FA",
+		},
+	},
+});
